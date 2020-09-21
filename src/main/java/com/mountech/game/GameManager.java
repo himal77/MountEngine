b7b036fe0327ca002918cad9fd4ca71602681d7a
himal77 <himalpuri6@gmail.com>
2020-09-21 17:04:28 +0200

    Inserting audio clip

--- src/main/java/com/mountech/game/GameManager.java
+++ src/main/java/com/mountech/game/GameManager.java
@@ -1,19 +1,30 @@
 package com.mountech.game;
 
+import com.mountech.audio.SoundClip;
 import com.mountech.engine.AbstractGame;
 import com.mountech.engine.GameContainer;
 import com.mountech.engine.Renderer;
 import com.mountech.gfx.ImageTile;
 
+import java.awt.event.KeyEvent;
+
 public class GameManager extends AbstractGame {
     private ImageTile image;
+    private SoundClip clip;
+
     private float temp = 0;
 
     public GameManager() {
         image = new ImageTile("/test.png", 16, 16);
+        clip = new SoundClip("/audio/welcome.wav");
     }
 
     public void update(GameContainer gc, float dt) {
+
+        if(gc.getInput().isKeyDown(KeyEvent.VK_A)) {
+            clip.play();
+        }
+
         temp += dt * 2;
         if(temp == 3) temp = 0;
     }
